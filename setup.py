ac2150832bab0880e7267c249f3b11de0fcd889e
Jonathan Robey <class4kayaker@gmail.com>
2019-12-19 20:53:47 -0700

    Update setuptools config

--- setup.py
+++ setup.py
@@ -2,23 +2,6 @@
 Script to fetch citation data given DOIs
 """
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
-package = 'fetch_cite'
-version = '0.1.0'
-dependencies = [
-    'click',
-    'requests'
-]
-
-setup(
-    name=package,
-    version=version,
-    packages=find_packages(),
-    install_requires=dependencies,
-    entry_points={
-        'console_scripts': [
-            'fetch_cite = fetch_cite.cli:fetch'
-        ]
-    }
-)
+setup()
